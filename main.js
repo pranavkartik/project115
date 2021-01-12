0659970809b98aa302cc184c2ec9aadd608554ce
pranavkartik <71919321+pranavkartik@users.noreply.github.com>
2021-01-12 14:39:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+function preload() {
+    clown_nose = loadImage("https://i.postimg.cc/7ZBcjDqp/clownnose.png");
+}
+
+function setup() {
+    canvas = createCanvas(300, 250);
+    canvas.position(530, 150);
+    video = createCapture(VIDEO);
+    video.size(300, 250);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+}
+
+function draw() {
+    image(video, 0, 0, 300, 250);
+}
+
+function take_snapshot() {
+    save('my_moustache_filter.png');
+}
+
+function modelLoaded() {
+    console.log("PoseNet is initialized");
+}
+noseX = 0;
+noseY = 0;
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+    }
+}
